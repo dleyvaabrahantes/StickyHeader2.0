382446d7ec42de31193dc0f677cbdf5b7dc2aee9
David Leyva <dleyvaabrahantes@gmail.com>
2024-03-18 12:59:21 -0400

    Full header

--- StickyHeader2.0/ContentView.swift
+++ StickyHeader2.0/ContentView.swift
@@ -9,53 +9,129 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
+   
 
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
+        ScrollView{
+            VStack{
+                imageView()
+                GeometryReader { geo in
+                    let minY = geo.frame(in: .global).minY
+                    HStack{
+                        Button {
+                            
+                        } label: {
+                            Label("Email", systemImage: "message")
+                                .font(.callout)
+                                .bold()
+                                .foregroundStyle(.black)
+                                .frame(width: 240, height: 45)
+                                .background(
+                                    .ultraThinMaterial, in:RoundedRectangle(cornerRadius: 30)
+                                )
+                        }
+
+                        Cbutton(iconName: .in, action: {})
+                        Cbutton(iconName: .x, action: {})
                     }
+                    .frame(maxWidth: .infinity)
+                    .offset(y: max(60 - minY, 0))
                 }
-                .onDelete(perform: deleteItems)
+                .offset(y: -50)
+                .zIndex(1)
+                GridView()
             }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
         }
+        .ignoresSafeArea()
     }
 
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
-        }
-    }
+    
 }
 
 #Preview {
     ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
+       
+}
+
+@ViewBuilder
+func imageView() -> some View {
+    GeometryReader { geo in
+        let minY = geo.frame(in: .global).minY
+        let isScrolling = minY > 0
+        VStack{
+            Image(.realstate)
+                .resizable()
+                .scaledToFill()
+                .frame(height: isScrolling ? 160 + minY : 160)
+                .clipped()
+                .offset(y: isScrolling ? -minY : 0)
+                .blur(radius: isScrolling ? 0 + minY / 80 : 0)
+                .scaleEffect(isScrolling ? 1 + minY / 2000 : 1)
+                .overlay(alignment: .bottom) {
+                    ZStack{
+                        Image(.realstate)
+                            .resizable()
+                            .scaledToFill()
+                        Circle().stroke(lineWidth: 6)
+                    }
+                    .frame(width: 110, height: 110)
+                    .clipShape(Circle())
+                    .offset(y: 50)
+                    .offset(y: isScrolling ? -minY : 0)
+                }
+            Group{
+                VStack{
+                    Text("DLEYVAA")
+                        .bold()
+                        .font(.title)
+                    Text("I started learning swiftui a year and a half ago, and i loved it very much, I like sharing what i learned on Youtube")
+                        .font(.callout)
+                        .multilineTextAlignment(.center)
+                        .frame(width: geo.size.width - 30)
+                        .lineLimit(3)
+                        .fixedSize()
+                }
+                .offset(y: isScrolling ? -minY : 0)
+            }
+            .padding(.vertical, 53)
+        }
+    }
+    .frame(height: 385)
+    
+}
+
+
+struct GridView: View {
+    var body: some View {
+        LazyVGrid(columns: Array(repeating: GridItem(), count: 2)) {
+            ForEach(0 ..< 25){ item in
+                RoundedRectangle(cornerRadius: 20)
+                    .frame(width: 180, height: 220)
+                    .foregroundStyle(.ultraThinMaterial)
+            }
+        }
+        .padding(.horizontal,10)
+    }
+}
+
+struct Cbutton: View {
+    let iconName: UIImage
+    var action:()-> Void
+    
+    var body: some View {
+        Button{
+            action()
+        }label: {
+            Image(uiImage: iconName)
+                .resizable()
+                .scaledToFill()
+                .frame(width: 23, height: 23)
+                .padding(10)
+                .background(.ultraThinMaterial, in: Circle())
+                .overlay {
+                    Circle().stroke(lineWidth: 1)
+                        .foregroundStyle(LinearGradient(gradient: Gradient(colors: [.black, .gray]), startPoint: .bottomLeading, endPoint: .topTrailing))
+                }
+        }
+    }
 }
